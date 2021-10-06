b0b554b8ff70189af659f5a1b5b59f145032a40a
Lisa Mac <lisa@MacBook-Air-2.local>
2021-10-06 18:08:12 +0100

    POST/redirect/GET pattern

--- app.rb
+++ app.rb
@@ -2,6 +2,7 @@ require 'sinatra/base'
 require 'sinatra/reloader'
 
 class Battle < Sinatra::Base
+  enable :sessions
   configure :development do
     register Sinatra::Reloader
   end
@@ -11,9 +12,15 @@ class Battle < Sinatra::Base
   end 
 
   post '/names' do 
-    @player_1_name = params[:player_1_name]
-    @player_2_name = params[:player_2_name]
-    erb :play
+    session[:player_1_name] = params[:player_1_name]
+    session[:player_2_name] = params[:player_2_name]
+    redirect '/play'
+  end 
+
+  get '/play' do 
+    @player_1_name = session[:player_1_name] #params doesn't work here because you get that with get request only 
+    @player_2_name = session[:player_2_name]
+    erb :play 
   end 
 
   # start the server if ruby file executed directly

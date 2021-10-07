40d23466e5f7ed64e87cc5fe797063ce29d32fe7
Tom <t.p.kellett@gmail.com>
2021-10-07 16:34:51 +0100

    skinny controllers page

--- spec/unit/game_spec.rb
+++ spec/unit/game_spec.rb
@@ -1,14 +1,26 @@
 require 'game'
 
 describe Game do 
-  # subject(:charlotte) { Player.new('Charlotte') }
-  # subject(:mittens) { Player.new('Mittens') }
   let(:charlotte) { double("Player 1") }
+  let(:mittens) { double("Player 1") }
+  let(:game) { described_class.new(charlotte, mittens)}
 
   describe '#attack' do
     it 'damages the player' do
       expect(charlotte).to receive(:receive_damage)
-      subject.attack(charlotte)
+      game.attack(charlotte)
+    end
+  end
+
+  describe "#player1" do
+    it "should retrieve player1" do
+      expect(game.player_1).to eq(charlotte)
+    end
+  end
+
+  describe "#player2" do
+    it "should retrieve player 2" do
+      expect(game.player_2).to eq(mittens)
     end
   end
 end 

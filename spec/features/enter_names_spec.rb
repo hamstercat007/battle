60fc976af1a6783c7189c3ddbad6a49a512af945
Tom <t.p.kellett@gmail.com>
2021-10-07 16:01:59 +0100

    removing unnecessary html files

--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -2,7 +2,5 @@ feature 'Enter names' do
   scenario 'submitting names' do
     sign_in_and_play
     expect(page).to have_content 'Charlotte vs. Mittens'
-
-    save_and_open_page # will save the web page and open the browser to display it
   end
 end
